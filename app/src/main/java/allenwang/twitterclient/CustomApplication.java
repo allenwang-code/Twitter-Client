e70fe56888804b24c78b72efa7d60f3da0c59b73
AllenWang <s972024@gmail.com>
2017-03-02 23:59:46 +0800

    try to fix login issue

---
+++ app/src/main/java/allenwang/twitterclient/CustomApplication.java
@@ -0,0 +1,28 @@
+package allenwang.twitterclient;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by allenwang on 2017/3/2.
+ */
+
+public class CustomApplication extends Application {
+
+    private static String TWITTER_KEY;
+    private static String TWITTER_SECRET;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TWITTER_KEY = getString(R.string.twitter_api_key);
+        TWITTER_SECRET = getString(R.string.twitter_api_key);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
--- app/src/main/java/allenwang/twitterclient/MainActivity.java
+++ app/src/main/java/allenwang/twitterclient/MainActivity.java
@@ -4,20 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-import io.fabric.sdk.android.Fabric;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG  = MainActivity.class.getSimpleName();
-    private static String TWITTER_KEY;
-    private static String TWITTER_SECRET;
 
     private TwitterLoginButton loginButton;
 
@@ -26,13 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TWITTER_KEY = getString(R.string.twitter_api_key);
-        TWITTER_SECRET = getString(R.string.twitter_api_key);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
-
         loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
-        loginButton.setEnabled(true);
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
