e8672f84e2e51549bc199637548e3c15afeedb46
AllenWang <s972024@gmail.com>
2017-03-02 21:01:15 +0800

    set up grade

--- app/src/main/java/allenwang/twitterclient/MainActivity.java
+++ app/src/main/java/allenwang/twitterclient/MainActivity.java
@@ -1,13 +1,25 @@
 package allenwang.twitterclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TWITTER_KEY = "";
+    private static final String TWITTER_SECRET = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
     }
 }
